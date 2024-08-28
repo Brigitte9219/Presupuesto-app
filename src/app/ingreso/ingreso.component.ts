c70af4bc5a26c65ba8d8f9b1ca150ce6da005814
BrigittePadilla <martha.padilla@croydon.com.co>
2024-08-28 13:19:01 -0500

    Modificando componente de ingresos

--- src/app/ingreso/ingreso.component.ts
+++ src/app/ingreso/ingreso.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IngresoService } from './ingreso.service';
+import { Ingreso } from './ingreso';
 
 @Component({
   selector: 'app-ingreso',
   templateUrl: './ingreso.component.html',
   styleUrl: './ingreso.component.css'
 })
-export class IngresoComponent {
+export class IngresoComponent implements OnInit{
+
+  ingresos: Ingreso[]=[];
+
+  constructor(private ingresoService: IngresoService){
+  }
+  ngOnInit(): void {
+    this.ingresos = this.ingresoService.ingresos;
+  }
+
+  eliminarRegistro(ingreso: Ingreso){
+    this.ingresoService.eliminar(ingreso);
+  }
+
 
 }
